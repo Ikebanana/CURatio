fa6d3bdfd35991d7a0627d8b7e3a32f9c6b5321c
Joanna Cholewo <joanna.cholewo@gmail.com>
2021-11-10 18:27:12 -0500

    Reindent (whitespace-only changes)

--- CURatio/CURatio.R
+++ CURatio/CURatio.R
@@ -2,54 +2,53 @@
 #  phyData: phyDat format data
 # consTree: consensus tree
 
-CURatio <- function(phyData, consTree){
-  stan.tree <- compute.brlen(consTree) 
-  # The tip labels of the consensus tree
-  stan.name <- consTree$tip.label
-  
-  if(length(phyData) > length(stdTree$tip.label)){
-    ratio <- NA
-  }
-  else if(length(phyData) == length(stdTree$tip.label)){
-    dm <- dist.hamming(phyData)
-    treeNJ <- NJ(dm)
-    fit <- pml(treeNJ,phyData)
-    treeFit <- optim.pml(fit, optNni=TRUE, model="JC")
-    b <- sum(treeFit$tree$edge.length)
-    
-    # STEP 2
-    
-    fit2 <- pml(stan.tree,phyData)
-    treeFit2 <- optim.pml(fit2, optNni=FALSE, model="JC") ### MLE tree under the JC model with constraint
-    B <- sum(treeFit2$tree$edge.length)
-    
-    ratio <- B/b
-  }
-  else if(3 < length(phyData) && length(phyData) < length(stdTree$tip.label)){
-    dm <- dist.hamming(phyData) 
-    treeNJ <- NJ(dm)
-    # Computing the likelihood of a phylogenetic tree given a sequence alignment and a model.
-    fit <- pml(treeNJ,phyData)
-    # Optimizing the different model parameters.
-    treeFit <- optim.pml(fit, optNni=TRUE, model="JC")
-    # 
-    b <- sum(treeFit$tree$edge.length)
-    
-    data.name <- attr(phyData,'names')
-    index <- which(stan.name %in% data.name)
-    remove.name <- stan.name[-index]
-    tree.new <- drop.tip(stan.tree,remove.name)
-    
-    # Computing the likelihood of a phylogenetic tree given a sequence alignment and a model.
-    fit2 <- pml(tree.new,phyData)
-    # Optimizing the different model parameters.
-    treeFit2 <- optim.pml(fit2, optNni=FALSE, model="JC") ### MLE tree under the JC model with constraint
-    # 
-    B<-sum(treeFit2$tree$edge.length)
-    
-    ratio = B/b
-  }
-  else {ratio <- NA}
-  
-  return(ratio)
+CURatio <- function(phyData, consTree) {
+    stan.tree <- compute.brlen(consTree)
+    # The tip labels of the consensus tree
+    stan.name <- consTree$tip.label
+
+    if (length(phyData) > length(stdTree$tip.label)) {
+        ratio <- NA
+    } else if (length(phyData) == length(stdTree$tip.label)) {
+        dm <- dist.hamming(phyData)
+        treeNJ <- NJ(dm)
+        fit <- pml(treeNJ, phyData)
+        treeFit <- optim.pml(fit, optNni=TRUE, model="JC")
+        b <- sum(treeFit$tree$edge.length)
+
+        # STEP 2
+
+        fit2 <- pml(stan.tree, phyData)
+        treeFit2 <- optim.pml(fit2, optNni=FALSE, model="JC") ### MLE tree under the JC model with constraint
+        B <- sum(treeFit2$tree$edge.length)
+
+        ratio <- B / b
+    } else if (3 < length(phyData) && length(phyData) < length(stdTree$tip.label)) {
+        dm <- dist.hamming(phyData)
+        treeNJ <- NJ(dm)
+        # Computing the likelihood of a phylogenetic tree given a sequence alignment and a model.
+        fit <- pml(treeNJ, phyData)
+        # Optimizing the different model parameters.
+        treeFit <- optim.pml(fit, optNni=TRUE, model="JC")
+        #
+        b <- sum(treeFit$tree$edge.length)
+
+        data.name <- attr(phyData, "names")
+        index <- which(stan.name %in% data.name)
+        remove.name <- stan.name[-index]
+        tree.new <- drop.tip(stan.tree, remove.name)
+
+        # Computing the likelihood of a phylogenetic tree given a sequence alignment and a model.
+        fit2 <- pml(tree.new, phyData)
+        # Optimizing the different model parameters.
+        treeFit2 <- optim.pml(fit2, optNni=FALSE, model="JC") ### MLE tree under the JC model with constraint
+        #
+        B <- sum(treeFit2$tree$edge.length)
+
+        ratio <- B / b
+    } else {
+        ratio <- NA
+    }
+
+    return(ratio)
 }
